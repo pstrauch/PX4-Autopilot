46b4caf21afc8eca388b1d21aca2a10b671d5482
Pascal Strauch <pstrauch@ethz.ch>
2026-08-06 11:41:53 +0200

    fix script paths

--- aerosim_trials.sh
+++ aerosim_trials.sh
@@ -4,8 +4,8 @@ export PX4_SIM_MODEL="aerosim_x500_v2"
 export PX4_SYS_AUTOSTART=22001
 
 make px4_sitl_aerosim
-cd /home/pstrauch/ws/PX4-Autopilot/build/px4_sitl_aerosim/rootfs
-/home/pstrauch/ws/PX4-Autopilot/build/px4_sitl_aerosim/bin/px4 -d
+cd $HOME/ws/PX4-Autopilot/build/px4_sitl_aerosim/rootfs
+$HOME/ws/PX4-Autopilot/build/px4_sitl_aerosim/bin/px4 -d
 
 unset PX4_SIM_MODEL
 unset PX4_SYS_AUTOSTART
